17ef6678a98dc9973821de2c3557e78c0fff06b7
hbsnow <yuki.4uing@gmail.com>
2018-04-13 00:48:07 +0900

    デザイン微調整

--- src/assets/js/middleware/init.js
+++ src/assets/js/middleware/init.js
@@ -1,5 +1,6 @@
 import Document from '../class/Utils/Document'
 import Progress from '../components/Progress'
+import errorHtml from '../pug/error.pug'
 
 const progress = new Progress(document.getElementById('progress'))
 
@@ -13,7 +14,7 @@ export default async (ctx, next) => {
       ctx.state.content = await Document.fetch(`${ctx.canonicalPath}index.tpl`)
     } catch (error) {
       console.error(error)
-      ctx.state.content = `<p>データ取得に失敗したため、処理を中断しました。</p>`
+      ctx.state.content = errorHtml()
     }
     progress.finish()
   }
