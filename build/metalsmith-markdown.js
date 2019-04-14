799e61758d87fb507163842141a61a8707595eb4
hbsnow <yuki.4uing@gmail.com>
2019-04-14 21:41:08 +0900

    codeblockでファイル名がなかったときの表示が崩れる問題の修正

--- build/metalsmith-markdown.js
+++ build/metalsmith-markdown.js
@@ -23,12 +23,14 @@ module.exports = () => {
 
     return (
       `<div class="codeblock">` +
-      `<div class="codeblock__title">${fileName}</div>` +
-      `<pre class="codeblock__content ${ext}">` +
-      `<code class="codeblock__code codeblock__code--${ext}">` +
-      md.parser.utils.escapeHtml(token.content) +
-      `</code>` +
-      `</pre>\n` +
+        fileName && `<div class="codeblock__title">${fileName}</div>` +
+        `<pre class="codeblock__content` + (ext && ` ${ext}`) + `">` +
+          `<code class="codeblock__code` +
+            ext && `codeblock__code--${ext}` +
+          `">` +
+            md.parser.utils.escapeHtml(token.content) +
+          `</code>` +
+        `</pre>\n` +
       `</div>\n`
     )
   }
